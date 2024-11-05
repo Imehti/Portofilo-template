37c1856de3582b2881e33ba264dd989bca2a1e35
Imehti <khajavim497@gmail.com>
2024-11-05 08:04:20 +0330

    create home page and set bg for it

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className='relative p-6'>
+        <img className='rounded-2xl h-[26rem] w-full' src="/home-bg.jpg" alt="" />
+    </div>
+    </>
+  )
+}
+
+export default Home
