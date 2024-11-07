1361812ca2e8a94382a5fe1d98c7f84bf172c3b1
Imehti <khajavim497@gmail.com>
2024-11-07 23:48:19 +0330

    store projects details in an array in this file

---
+++ src/components/project-images.ts
@@ -0,0 +1,8 @@
+export const projectImages = [
+  { id: 1, src: "/software-design.jpg", content: "Software Design" ,group:'Computer'},
+  { id: 2, src: "/google-ai.jpg", content: "Google Ai" ,group:'Computer'},
+  { id: 3, src: "/web-design.jpg", content: "Web Design" ,group:'Computer'},
+  { id: 4, src: "/strength-materials.webp",content: "Strength Of Materials" ,group:'Mechanic'},
+  { id: 5, src: "/cloud-space.jpg", content: "Cloud Space" ,group:'Computer'},
+  { id: 6, src: "/blockchain.jpg", content: "Blockchain Technology",group:'Computer' },
+];
