a21e24808913ae865528f51d0ae16894ab83b4ad
Imehti <khajavim497@gmail.com>
2024-11-07 23:47:43 +0330

    createportofilo section

--- src/components/Portofilo.tsx
+++ src/components/Portofilo.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import ProjectLinks from "./ProjectLinks";
 
 const Portofilo = () => {
+    
   return (
-    <div>Portofilo</div>
-  )
-}
+    <>
+      <div className="mt-20">
+        <div className="flex flex-col justify-center space-y-2 items-center">
+          <h2 className="font-serif text-blue-600">Our Portofilo </h2>
+          <h1 className="text-black text-2xl font-serif">
+            Our Recent Projects
+          </h1>
+          <p className="max-w-lg text-center font-light">
+            There are many variations of passages of Lorem Ipsum available but
+            the majority have suffered alteration in some form
+          </p>
+        </div>
+        <ProjectLinks />
+      </div>
+    </>
+  );
+};
 
-export default Portofilo
+export default Portofilo;
