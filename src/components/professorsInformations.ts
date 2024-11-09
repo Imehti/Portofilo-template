d77474b42f47a888cfe627b4f3e692ca577a5e62
Imehti <khajavim497@gmail.com>
2024-11-09 15:59:00 +0330

    store professors information

---
+++ src/components/professorsInformations.ts
@@ -0,0 +1,9 @@
+
+export const  professorsInfos=[
+    {id:1,image:'/profile card img.png',name:'Rasool Taghipoor',title:'Multidisciplinary Enterpreneur',group:'computer',status:'Active',role:'Head',email:'example@gmmail.com'},
+    {id:2,image:'/profile card img.png',name:'Hossein Monshi Zade',title:'Regional Paradigm Technecian',group:'computer',status:'Active',role:'Prof',email:'example@gmmail.com'},
+    {id:1,image:'/profile card img.png',name:'Mohsen Nokhodchian',title:'Regional Paradigm Technecian',group:'computer',status:'Active',role:'Prof',email:'example@gmmail.com'},
+    {id:1,image:'/profile card img.png',name:'Mohsen Heydari',title:'Lead Implementation Liasion',group:'computer',status:'Active',role:'Prof',email:'example@gmmail.com'},
+    {id:1,image:'/profile card img.png',name:'Ali Mosavi',title:'Applications Engineer',group:'computer',status:'Deactive',role:'Programmer',email:'example@gmmail.com'},
+    {id:1,image:'/profile card img.png',name:'Rasool Taghipoor',title:'Regional Paradigm Technecian',group:'computer',status:'Active',role:'Prof',email:'example@gmmail.com'},
+]
