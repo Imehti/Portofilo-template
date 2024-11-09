fe38f664d755d02693e20b3af025366366f42705
Imehti <khajavim497@gmail.com>
2024-11-09 15:25:37 +0330

    create an array of footer links annd list them

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,55 @@
 import React from "react";
 
+const footerLinks = [
+  { title: 'Resources', links: ['Sass Development', 'Our Products', 'Our Projects', 'User  Strategy'] },
+  { title: 'Company', links: ['About Landio', 'Contact & Support', 'Success History', 'Setting & Privacy'] },
+  { title: 'Quick Links', links: ['Premium Support', 'Our Services', 'Know Our Team', 'Download App'] },
+];
+
 const Footer = () => {
   return (
     <>
-    <div className="mt-10">
-      <div className="flex justify-around items-center">
-        <ul>
-          <li>
-            <h1 className="font-extrabold">
-              Ar<span className="text-blue-800">chi</span>vi
-            </h1>
-          </li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
+      <div className="mt-10">
+        <div className="">
+          <div className="flex justify-around p-8">
+            <div className="flex flex-col space-y-4">
+              <h1 className="font-extrabold">
+                Ar<span className="text-blue-800">chi</span>vi
+              </h1>
+              <p className="max-w-sm font-light">
+                There are many variations of passages of Lorem Ipsum available
+                but the majority have suffered alteration in some form.
+              </p>
+              <div className="flex space-x-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
+                  />
+                </svg>
+                <p className="font-normal">0512335787</p>
+              </div>
+            </div>
+            {footerLinks.map((link, index) => (
+              <div className="flex flex-col space-y-3" key={index}>
+                <h2 className="font-bold">{link.title}</h2>
+                <ul className="font-thin">
+                  {link.links.map((subLink, subIndex) => (
+                    <li className="hover:text-blue-700 cursor-pointer" key={subIndex}>{subLink}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
