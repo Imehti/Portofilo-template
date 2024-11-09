77978dca3a28f9233a4a5721cfe1b0b69a42ceb0
Imehti <khajavim497@gmail.com>
2024-11-09 13:39:22 +0330

    create this div to display titles in a flex way

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <div className='flex justify-around items-center'>
+
+    </div>
+    </>
   )
 }
 
