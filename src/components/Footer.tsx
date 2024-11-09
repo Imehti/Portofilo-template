c7e99fa6053a2dba0755f6d963f39ad50f5b1cc9
Imehti <khajavim497@gmail.com>
2024-11-09 13:41:56 +0330

    add a container div and put other parts in this container

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <>
-    <div className='flex justify-around items-center'>
-
-    </div>
+    <div className="mt-10">
+      <div className="flex justify-around items-center">
+        <ul>
+          <li>
+            <h1 className="font-extrabold">
+              Ar<span className="text-blue-800">chi</span>vi
+            </h1>
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
