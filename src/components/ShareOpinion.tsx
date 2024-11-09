afe8a3b8c276c868a1b15050e07282a1ea07ac74
Imehti <khajavim497@gmail.com>
2024-11-09 23:22:39 +0330

    make opinion section responsive

--- src/components/ShareOpinion.tsx
+++ src/components/ShareOpinion.tsx
@@ -10,33 +10,33 @@ const ShareOpinion = () => {
           alt=""
         />
         <div className="absolute flex flex-col items-center space-y-3 text-white mt-12">
-          <h1 className="text-4xl font-serif">Share your thoughts with us</h1>
-          <p className="max-w-lg text-center font-light">
+          <h1 className="text-xl lg:text-4xl font-serif">Share your thoughts with us</h1>
+          <p className="md:max-w-lg max-w-xs text-center font-light">
             There are many variations of passages of Lorem Ipsum available but
             the majority have suffered alteration in some form
           </p>
 
           <form action="">
             <div className="flex flex-col items-center mt-6 space-y-2">
-              <label className="text-xl font-serif" htmlFor="">
+              <label className="md:text-xl font-serif" htmlFor="">
                 Name
               </label>
               <input
-                className="rounded-md shadow-lg p-1.5 px-14"
+                className="rounded-md shadow-lg p-0.3 md:p-1.5 md:px-14"
                 type="text"
                 placeholder="David John"
               />
-              <label className="text-xl font-serif" htmlFor="">
+              <label className="md:text-xl font-serif" htmlFor="">
                 Email
               </label>
               <input
-                className="rounded-md shadow-lg p-1.5 px-14"
+                className="rounded-md shadow-lg p-0.3 md:p-1.5 md:px-14"
                 type="text"
                 placeholder="DavidJohn@gmail.com"
               />
             </div>
             <div className="flex justify-center mt-10">
-              <button className="bg-blue-700 text-sm p-3 rounded-md">
+              <button className="bg-blue-700 text-sm p-1.5 md:p-3 rounded-md">
                 Go to the comment form
               </button>
             </div>
